26d4ad281c1e8cd0452a4a5c30c4836672bf66a2
Aman Singh <57554151+TerMinator-spec@users.noreply.github.com>
2021-01-05 18:55:27 +0530

    Update modulus.py

--- portfolio management/modulus.py
+++ portfolio management/modulus.py
@@ -71,24 +71,7 @@ def var_historic(r, level=5):
 
 
 from scipy.stats import norm
-def var_gaussian(r, level=5, modified=False):
-    """
-    Returns the Parametric Gauusian VaR of a Series or DataFrame
-    If "modified" is True, then the modified VaR is returned,
-    using the Cornish-Fisher modification
-    """
-    # compute the Z score assuming it was Gaussian
-    z = norm.ppf(level/100)
-    if modified:
-        # modify the Z score based on observed skewness and kurtosis
-        s = skewness(r)
-        k = kurtosis(r)
-        z = (z +
-                (z**2 - 1)*s/6 +
-                (z**3 -3*z)*(k-3)/24 -
-                (2*z**3 - 5*z)*(s**2)/36
-            )
-    return -(r.mean() + z*r.std(ddof=0))
+
 
 def cvar_historic(r, level=5):
     """
@@ -194,33 +177,7 @@ def optimal_weights(n_points, er, cov):
     weights = [minimize_vol(target_return, er, cov) for target_return in target_rs]
     return weights
 
-def msr(riskfree_rate, er, cov):
-    """
-    Returns the weights of the portfolio that gives you the maximum sharpe ratio
-    given the riskfree rate and expected returns and a covariance matrix
-    """
-    n = er.shape[0]
-    init_guess = np.repeat(1/n, n)
-    bounds = ((0.0, 1.0),) * n # an N-tuple of 2-tuples!
-    # construct the constraints
-    weights_sum_to_1 = {'type': 'eq',
-                        'fun': lambda weights: np.sum(weights) - 1
-    }
-    def neg_sharpe(weights, riskfree_rate, er, cov):
-        """
-        Returns the negative of the sharpe ratio
-        of the given portfolio
-        """
-        r = portfolio_return(weights, er)
-        vol = portfolio_vol(weights, cov)
-        return -(r - riskfree_rate)/vol
-    
-    weights = minimize(neg_sharpe, init_guess,
-                       args=(riskfree_rate, er, cov), method='SLSQP',
-                       options={'disp': False},
-                       constraints=(weights_sum_to_1,),
-                       bounds=bounds)
-    return weights.x
+
 
 def plot_ef(n_points, er, cov, style='.-', legend=False, show_cml=False, riskfree_rate=0, show_ew=False, show_gmv=False):
     """
@@ -234,36 +191,7 @@ def plot_ef(n_points, er, cov, style='.-', legend=False, show_cml=False, riskfre
         "Volatility": vols
     })
     ax = ef.plot.line(x="Volatility", y="Returns", style=style, legend=legend)
-    if show_cml:
-        ax.set_xlim(left = 0)
-        # get MSR
-        w_msr = msr(riskfree_rate, er, cov)
-        r_msr = portfolio_return(w_msr, er)
-        vol_msr = portfolio_vol(w_msr, cov)
-        # add CML
-        cml_x = [0, vol_msr]
-        cml_y = [riskfree_rate, r_msr]
-        ax.plot(cml_x, cml_y, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=10)
-    if show_ew:
-        n = er.shape[0]
-        w_ew = np.repeat(1/n, n)
-        r_ew = portfolio_return(w_ew, er)
-        vol_ew = portfolio_vol(w_ew, cov)
-        # add EW
-        ax.plot([vol_ew], [r_ew], color='goldenrod', marker='o', markersize=10)
-    if show_gmv:
-        w_gmv = gmv(cov)
-        r_gmv = portfolio_return(w_gmv, er)
-        vol_gmv = portfolio_vol(w_gmv, cov)
-        # add EW
-        ax.plot([vol_gmv], [r_gmv], color='midnightblue', marker='o', markersize=10)
-        
-        return ax
     
-def gmv(cov):
-    """
-    Returns the weights of the Global Minimum Volatility portfolio
-    given a covariance matrix
-    """
-    n = cov.shape[0]
-    return msr(0, np.repeat(1, n), cov)
+    return ax
+    
+
